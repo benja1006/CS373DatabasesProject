1493a2c33952fc2b8187ed8ea93afd89528c51ca
benja <benja1006@gmail.com>
2022-12-11 14:23:32 -0800

    updated run so that it opens web browser after waiting. Query form good

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 docker compose up --wait
-docker exec -it tournament_tracker /bin/bash
+echo "Waiting for server to be ready..."
+for i in 1 2 3 4 5 6 7 8 9
+do
+echo "$i"
+sleep 1
+done
+open http://localhost:3000/
